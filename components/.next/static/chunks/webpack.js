e1738d09e162f2464d325a4d8f558058af47cfec
Daloniil <62095873+Daloniil@users.noreply.github.com>
2022-04-04 22:51:41 +0300

    dfix tsconfig

--- components/.next/static/chunks/webpack.js
+++ components/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "48d3ab592a3cc8d4"; }
+/******/ 		__webpack_require__.h = function() { return "2e93e0af48e22354"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
