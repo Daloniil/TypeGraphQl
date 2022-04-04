0a68165d2bab90e1f395f06c7a9485b89ebc9ed6
Daloniil <62095873+Daloniil@users.noreply.github.com>
2022-04-04 22:55:19 +0300

    add types

--- components/pages/index.tsx
+++ components/pages/index.tsx
@@ -1,14 +1,16 @@
 import type { NextPage } from "next";
-import {  useGetTasksQuery } from "../generated";
+import { useGetTasksQuery } from "../generated";
 
 const Home: NextPage = () => {
-  const { data } =  useGetTasksQuery();
+  const { data } = useGetTasksQuery();
 
-
-
-
-  return <div>{data ? data.getTasks.map(task => (<p key={task.id}>{task.title}</p> )):''}</div>;
+  return (
+    <div>
+      {data
+        ? data.getTasks.map((task) => <p key={task.id}>{task.title}</p>)
+        : ""}
+    </div>
+  );
 };
 
 export default Home;
-
