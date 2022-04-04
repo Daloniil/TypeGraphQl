e6466b5aa9590360b5907696ea25fe68ff292424
Daloniil <62095873+Daloniil@users.noreply.github.com>
2022-04-04 21:31:19 +0300

    typegraphql && next Js && Apollo

---
+++ server/dist/ormconfig.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ormConfig = void 0;
+exports.ormConfig = {
+    type: "postgres",
+    database: "typeGraphQl",
+    entities: [],
+    logging: true,
+    synchronize: true,
+    username: "daloniil",
+    password: "daloniil",
+    port: 5432,
+};
+//# sourceMappingURL=ormconfig.js.map
---
+++ server/src/resolvers/task.ts
@@ -0,0 +1,54 @@
+import { Task } from "./../entity/Task";
+import { Arg, Int, Mutation, Query, Resolver } from "type-graphql";
+
+@Resolver()
+export class TaskResolver {
+  @Query(() => [Task])
+  getTasks(): Promise<Task[]> {
+    return Task.find({});
+  }
+
+  @Query(() => String)
+  hello(): string {
+    return "Hello";
+  }
+
+  @Query(() => Task, { nullable: true })
+  getTask(@Arg("id", () => Int) id: number): Promise<Task | null> {
+    return Task.findOne({ where: { id } });
+  }
+
+  @Mutation(() => Boolean)
+  deleteTask(@Arg("id", () => Int) id: number): boolean {
+    try {
+      Task.delete({ id });
+      return true;
+    } catch {
+      return false;
+    }
+  }
+
+  @Mutation(() => Boolean)
+  createTask(@Arg("title", () => String) title: string): Promise<Task> {
+    return Task.create({ title, isComplete: false }).save();
+  }
+
+  @Mutation(() => String, { nullable: true })
+  async updateTask(
+    @Arg("id", () => Int) id: number,
+    @Arg("title", () => String) title: string
+  ): Promise<string | null> {
+    const task = await Task.findOne({ where: { id } });
+
+    if (!task) {
+      return null;
+    }
+
+    try {
+      Task.update({ id }, { title });
+      return title;
+    } catch {
+      return null;
+    }
+  }
+}
