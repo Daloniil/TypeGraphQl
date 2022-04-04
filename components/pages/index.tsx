aad94ce7b3b640170b879647ebdf7d7ee4d548c8
Daloniil <62095873+Daloniil@users.noreply.github.com>
2022-04-04 22:04:48 +0300

    edit client

---
+++ components/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+import {  useGetTasksQuery } from "../generated";
+
+const Home: NextPage = () => {
+  const { data } =  useGetTasksQuery();
+
+
+
+
+  return <div>{data ? data.getTasks.map(task => (<p key={task.id}>{task.title}</p> )):''}</div>;
+};
+
+export default Home;
+
